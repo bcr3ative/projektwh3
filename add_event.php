e40c179b4fb83dce7508741e7140035f807c59ad
Paolo Perkovic <paolo.perkovic@gmail.com>
2013-10-19 16:52:50 +0200

    event, category

--- add_event.php
+++ add_event.php
@@ -2,33 +2,33 @@
 	include_once 'config.php';
 
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$event=$_POST['event'];
-		$opis=$_POST['opis'];
-		$lon=$_POST['lon'];
-		$lat=$_POST['lat'];
-		$numdays=$_POST['numdays'];
-		$days=$_POST['days'];
-		$date=$_POST['datepicker'];
-
 		if (isset($_SESSION['user']) && isset($_SESSION['nickname'])) {
-			db_connect();
-			$user=$_SESSION['user'];
-			$result=mysqli_query($mysqli, "SELECT id FROM korisnik WHERE mail = '$user';");
-			$data=mysqli_fetch_array($result);
-			$autor=$data['id'];
-
-			if(empty($_POST['event']) || empty($_POST['lon']) || empty($_POST['lat']) || empty($_POST['numdays']) || empty($_POST['days'])) {
-
+			if (!isset($_POST['event']) || !isset($_POST['lon']) || !isset($_POST['lat']) || !isset($_POST['datepicker']) || !isset($_POST['category']) || !isset($_POST['type'])) {
 			} else {
-				if(empty($_POST['days'])) {
+				$event=$_POST['event'];
+				$opis=$_POST['opis'];
+				$lon=$_POST['lon'];
+				$lat=$_POST['lat'];
+				$numdays=$_POST['numdays'];
+				$date=$_POST['datepicker'];
+				$category=$_POST['category'];
+				$type=$_POST['type'];
+
+				db_connect();
+				$user=$_SESSION['user'];
+				$result=mysqli_query($mysqli, "SELECT id FROM korisnik WHERE mail = '$user';");
+				$data=mysqli_fetch_array($result);
+				$autor=$data['id'];
+
+				if(!isset($numdays)) {
 					$numdays=0;
 				}
-					mysqli_query($mysqli, "INSERT INTO `event` (`event`, `opis`, `lon`, `lat`, `numdays`, `vrijeme`, `autor`) 
-						VALUES ('$event', '$opis', '$lon', '$lat', '$numdays', '$date', '$autor');");
-					$id=mysqli_insert_id($mysqli);
-				
+				echo $date;
+				mysqli_query($mysqli, "INSERT INTO event (naziv, opis, lon, lat, broj_dana, vrijeme, autor, id_kategorija, id_tip) VALUES ('$event', '$opis', '$lon', '$lat', '$numdays', 'FROM_UNIXTIME($date)', '$autor', '$category', '$type');");
+				$id=mysqli_insert_id($mysqli);
+				db_disconnect();
 			}
-			db_disconnect();
+			
 		}
 	}	
 ?>
@@ -41,20 +41,46 @@
 	<script src="js/jquery-ui.js"></script>
 	<script>
 		$(function() {
-		$( "#datepicker" ).datepicker();
+			$('#numdays').hide();
+			$("#datepicker").datepicker();
+			$('#multiday').change(function() {
+			  if ($(this).prop('checked')) {
+			    $('#numdays').show();
+			  } else {
+			    $('#numdays').hide();
+			  }
+			});
+			$('#category').change(function() {
+			  $('#type').append($('#type').load('get_cat.php?category='+$('#category').val()));
+			});
 		});
 	</script>
 </head>
 <body>
-	<form action="index.php" method="POST">
+	<form action="add_event.php" method="POST">
 		<input type="text" name="event" placeholder="Naziv"><br>
 		<input type="text" name="opis" placeholder="Opis"><br>
 		<input type="text" name="lon" placeholder="Širina"><br>
 		<input type="text" name="lat" placeholder="Dužina"><br>
-		<input type="checkbox" id="multiday" name="days"><br>
+		Višednevno: <input type="checkbox" id="multiday"><br>
 		<input type="text" id="numdays" name="numdays" placeholder="Broj dana"><br>
-		<input type="submit" name="submit" value="Upisi event">
-		<p>Date: <input type="text" id="datepicker" /></p>
+		<p>Datum: <input type="text" id="datepicker" name="datepicker"/></p>
+		Kategorija: 
+		<select name="category" id="category">
+			<option value="default" disabled="disabled" selected="selected">Odaberi</option>
+			<?php
+			    db_connect();
+				 if ($pretraga = $mysqli->query("SELECT id, ime FROM kategorija;")) { 
+        		 	while($row = $pretraga->fetch_array()){ 
+            			echo '<option value="'.$row['id'].'">'.$row['ime'].'</option>';
+        			} 
+   				}
+    			db_disconnect();
+			?>
+		</select><br>
+		Tip: 
+		<span id="type"></span><br>
+		<input type="submit" name="submit" value="Dodaj event">
 	</form>
 </body>
 </html>
---
+++ get_cat.php
@@ -0,0 +1,22 @@
+<?php
+	include_once 'config.php';
+
+	if (isset($_GET['category'])) {
+		$category=$_GET['category'];
+	} else {
+		die();
+	}
+
+	$type=Array();
+
+	db_connect();
+	$query=mysqli_query($mysqli, "SELECT id, ime FROM tip WHERE id_kategorija = '$category';");
+	db_disconnect();
+
+	echo "<select name='type'>";
+	while ($data=mysqli_fetch_array($query)) {
+		$type[$data['id']]=$data['ime'];
+		echo '<option value="'.$data['id'].'">'.$data['ime'].'</option>';
+	}
+	echo "</select>";
+?>
