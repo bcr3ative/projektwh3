a74cf6f74351db95c06b5604ebc180089ddee856
root <root@iva-HP-Pavilion-g7-Notebook-PC.(none)>
2013-10-19 16:31:46 +0200

    napravljena stranica za evente

---
+++ event.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'config.php';
+?>
+
+<!DOCTYPE html> 
+<html>
+<head>
+	<title>Event</title>
+</head>
+<body>
+	<?php
+	db_connect();
+	if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+		$event=$_GET['e'];
+		$about=mysqli_query($mysqli, "SELECT naziv, opis, vrijeme FROM event where id=$event");
+		while($row = $about->fetch_array()){ 
+			echo"$row[naziv]<br>";
+			echo"$row[opis]<br>";
+			echo"$row[vrijeme]<br>";
+		}
+	}
+	db_disconnect();
+?>
+</body>
+</html>
